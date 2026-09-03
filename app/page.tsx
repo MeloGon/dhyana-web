45c4d1d08d7cafa44a5087d0a6e088aab0ef9715
Kevyn Melo <troy.esname@gmail.com>
2026-09-02 23:27:22 -0500

    primer commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+ return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold">
+        Centro de Desarrollo Integral Dhyana
+      </h1>
+      <p className="text-lg text-gray-600">
+        Talleres grupales de psicología
+      </p>
+    </main>
+  );
+}
